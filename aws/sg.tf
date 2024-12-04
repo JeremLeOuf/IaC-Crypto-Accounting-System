8bf533334c7c391f8efe2c5356ef8abe9850cb14
Jerem <109222798+JeremLeOuf@users.noreply.github.com>
2024-12-04 15:14:58 +0100

    Update sg.tf

--- aws/sg.tf
+++ aws/sg.tf
@@ -1,12 +1,10 @@
-# Destroy the existing SG if it exists and recreate it
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
   description = "Allow EC2 instance to connect to other resources"
 
   lifecycle {
-    # Delete the existing SG if it exists before creating a new one
     create_before_destroy = true
-    ignore_changes        = [ingress, egress]  # Avoid unnecessary changes during>  }
+  }
 
   ingress {
     from_port   = 22
@@ -18,7 +16,7 @@ resource "aws_security_group" "ec2_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1" # -1 means all protocols
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
