5cf67bd40d0ce3d589a121463fdc1ec3741a641d
Jerem <109222798+JeremLeOuf@users.noreply.github.com>
2024-12-04 15:13:52 +0100

    Update install_terraform.sh

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,4 +1,5 @@
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
 wget -O- https://apt.releases.hashicorp.com/gpg | \
 gpg --dearmor | \
 sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
@@ -7,6 +8,9 @@ gpg --no-default-keyring \
 --fingerprint
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt-get install terraform
+
+sudo tee /etc/apt/sources.list.d/hashicorp.list && sudo apt update && sudo apt-get install terraform
+
+# Enable tab completion:
+touch ~/.bashrc
+terraform -install-autocomplete
