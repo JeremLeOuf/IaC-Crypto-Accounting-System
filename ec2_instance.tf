d6eb9b0be7d7798a42d62c660665dc63f88bb047
Jerem <jdjaoud@gmail.com>
2024-12-04 01:27:25 +0100

    Update ec2_instance

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0036618782d3fe37e"
   instance_type = "t2.micro"
   key_name      = var.ec2_key_name
   
