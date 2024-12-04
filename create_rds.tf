9dacba8a0e2d162d324e44fc4a3393a0bb3b4cfc
Jerem <jdjaoud@gmail.com>
2024-12-04 01:24:48 +0100

    Update rds

--- create_rds.tf
+++ create_rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "db_instance" {
   engine            = "postgres"
-  db_instance_class = "db.t3.micro"
+  instance_class = "db.t3.micro"
   allocated_storage = 20
   db_name           = "cryptocompta"
   username          = "admin"
