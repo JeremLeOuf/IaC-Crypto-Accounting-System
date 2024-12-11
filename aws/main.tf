e0d350c630c2c4230a69665437aa1ab90b1c76f3
JeremLeOuf <jdjaoud@gmail.com>
2024-12-12 00:45:43 +0100

    Updated secrets

--- aws/main.tf
+++ aws/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "CryptoCompta_HTW"
+    workspaces {
+      name = "IaC-Crypto-Accounting-System"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,6 +15,7 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
+
 provider "aws" {
   region = var.aws_region # Stockholm
 }
