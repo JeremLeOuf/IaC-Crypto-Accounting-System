4183938a412fb08ee99755477280111bb4e43156
JeremLeOuf <jdjaoud@gmail.com>
2024-12-12 20:43:03 +0100

    Reset repo

--- aws/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.CryptoCompta_EC2.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.CryptoCompta_EC2.public_ip
-}
--- aws/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-db_username   = "postgres"
-db_password   = "AKIAWQUOZLAYRHH5WCHY"
-db_name       = "cryptocompta"
-instance_type = "t3.micro"
